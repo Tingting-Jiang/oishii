bb9f2dcab9546e5907b76abd3df70e5379ff568b
Kiracus <abc.kiracus@gmail.com>
2021-12-08 23:34:07 -0800

    temporary commit for merge

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
             
-                    <Route path="/" element={<ExploreAndTrending/>}/>
+                    <Route path=["/", "/home"] element={<ExploreAndTrending/>}/>
                     <Route path="/login" element={<Login/>}/>
                     <Route path="/register" element={<Register/>}/>
                     <Route path="/profile" element={<Profile/>}/>
